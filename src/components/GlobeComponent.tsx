b29c1c0c4e75ce2787687ecddb8db1a11a02f85a
Efe Ebomwonyi <ebomwonyiefe@gmail.com>
2026-05-23 12:34:40 +0100

    style: sharpen control globe rings

--- src/components/GlobeComponent.tsx
+++ src/components/GlobeComponent.tsx
@@ -2027,18 +2027,18 @@ function ControlGlobeModel({ isSpinning }: { isSpinning: boolean }) {
       </mesh>
 
       <mesh>
-        <torusGeometry args={[0.865, 0.015, 8, 96]} />
-        <meshBasicMaterial color="#f7f9fc" transparent opacity={0.84} depthWrite={false} />
+        <torusGeometry args={[0.865, 0.024, 8, 96]} />
+        <meshBasicMaterial color="#ffffff" transparent opacity={0.94} depthWrite={false} />
       </mesh>
 
       <mesh scale={[1, 0.34, 1]}>
-        <torusGeometry args={[0.74, 0.012, 8, 96]} />
-        <meshBasicMaterial color="#d9fbff" transparent opacity={0.68} depthWrite={false} />
+        <torusGeometry args={[0.74, 0.018, 8, 96]} />
+        <meshBasicMaterial color="#eaffff" transparent opacity={0.82} depthWrite={false} />
       </mesh>
 
       <mesh rotation={[0, Math.PI / 2, 0]}>
-        <torusGeometry args={[0.82, 0.012, 8, 96]} />
-        <meshBasicMaterial color="#ffffff" transparent opacity={0.5} depthWrite={false} />
+        <torusGeometry args={[0.82, 0.018, 8, 96]} />
+        <meshBasicMaterial color="#ffffff" transparent opacity={0.72} depthWrite={false} />
       </mesh>
     </group>
   );
